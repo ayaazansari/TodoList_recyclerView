d9e38291e2a49867d4f34bcc092c160e905b3cb2
Ayaaz <ayaazansari3052@gmail.com>
2020-12-08 03:54:46 +0530

    end commit

--- app/src/main/java/com/example/newswithoutmvvm/MainActivity.kt
+++ app/src/main/java/com/example/newswithoutmvvm/MainActivity.kt
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_todo.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
+        var todoList = mutableListOf(
+            Todo("hehe",false))
+
+        val todoAdapter = TodoAdapter(todoList)
+        rvTodos.adapter = todoAdapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val title = editText.text.toString()
+            val todo= Todo(title,false)
+            todoList.add(todo)
+            todoAdapter.notifyDataSetChanged()
+        }
     }
 
 }
